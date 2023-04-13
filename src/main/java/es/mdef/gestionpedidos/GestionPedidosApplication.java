43fc0bd0e52739ae619b17e7aa7a3edeec250e3c
Javier Tomás Acín <javiko.tomas@gmail.com>
2023-04-13 13:03:49 +0200

    Actualizo las propiedades

--- src/main/java/es/mdef/gestionpedidos/GestionPedidosApplication.java
+++ src/main/java/es/mdef/gestionpedidos/GestionPedidosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionpedidosApplication {
+public class GestionPedidosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionpedidosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPedidosApplication.class, args);
+    }
 
 }
